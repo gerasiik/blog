7e32550b2b65f8fa28d0dca317537190ea892fa3
gerasiik <geraponce.siiksolutions@gmail.com>
2023-11-29 15:47:38 -0600

    configure la funcionalidad del blog

--- blog_coder/coderBlog/views.py
+++ blog_coder/coderBlog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
 
 # Create your views here.
-def index(request):
-    return render(request, 'coderBlog/index.html')
+# def index(request):
+#     return render(request, "coderBlog/index.html")
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = "coderBlog/index.html"
+
+
+class ArticleView(DetailView):
+    model = Post
+    template_name = "coderBlog/article.html"
